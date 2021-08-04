e64581a0acb8aa72f3375cd9939ca009744a78c6
saikarthikyadav139@gmail.com <saikarthikyadav139@gmail.com>
2021-08-04 14:53:38 +0200

    intial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+import { Navbar, NavbarBrand } from 'reactstrap';
+
+import Menu from './components/MenuComponent';
+
+function App() {
+  return (
+    <div>
+     <Navbar dark color = "primary" >
+       <div className = "container">
+         <NavbarBrand href="/">Sai Restautant</NavbarBrand>
+       </div>
+       </Navbar> 
+       <Menu/>
+    </div>
+  );
+}
+
+export default App;
